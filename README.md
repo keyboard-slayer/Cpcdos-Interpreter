c8993095292b914243044c2c57312eba46bcd5c0
0v3rl0w <dalcjor@libmail.eu>
2020-06-09 18:22:52 +0200

    Initial commit
    It can reads files !
    Awesome !!!

---
+++ README.md
@@ -0,0 +1,25 @@
+# Cpcdos Interpreter
+
+## Overview
+
+### Description
+
+This project is a small interpreter made for the sake of testing your CC+ scripts
+
+### Building the Project
+
+Run `cargo run` and voilà !
+
+### Usage
+
+Usage: `CPCDOS-Interpreter script` in a console terminal
+- where `script` is the relative or absolute path to the .cpc script
+
+### More about CC+
+
+CC+ is a programming language dedicated to distribution programming with the help of the CPCDOS project.
+
+Here is a list of revelant links for more details:
+- [CPCDOS' official website (in french)](https://cpcdos.net)
+- [CPCDOS' official Discord server (in french)](https://discord.gg/tMA8FeS)
+
---
+++ src/main.rs
@@ -0,0 +1,52 @@
+/*
+ * CC+ interpreter written in Rust
+ * Copyright (C) 2020 0v3rl0w & contributors
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+use std::{env, process, fs, io};
+use std::io::prelude::*;
+
+fn read_script(filename: &str) -> io::Result<String> {
+    let mut file = fs::File::open(filename)?;
+    let mut buf_reader = io::BufReader::new(file);
+    let mut script = String::new();
+    
+    buf_reader.read_to_string(&mut script)?;
+    Ok(script)
+}
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() == 1 {
+        eprintln!("Usage: {} FILE.cpc", args[0]);
+        process::exit(1);
+    }
+
+    let script = read_script(&args[1]);
+
+    match script {
+        Ok(content) => {
+            println!("{}", content);
+            //parse(&content);
+        },
+
+        Err(content) => {
+            eprintln!("{}", content);
+            process::exit(1);
+        },
+    }
+ }
